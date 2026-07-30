9c4710ae14bbb8462046d6d27f39027c96ce221c
tuturam <ahmadrama251005@gmail.com>
2026-07-30 15:30:12 +0700

    feat: add maptiler geocoding control

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -2,6 +2,7 @@
 import { config, MapStyle, Map, geocoding } from '@maptiler/sdk'
 import '@maptiler/sdk/dist/maptiler-sdk.css';
 import { onMounted, onUnmounted, shallowRef } from 'vue';
+import { GeocodingControl } from '@maptiler/geocoding-control/maptilersdk';
 
 const mapContainer = shallowRef<HTMLElement | null>(null);
 const map = shallowRef<Map | null>(null);
@@ -20,8 +21,22 @@ onMounted( async () => {
     container: mapContainer.value,
     style: MapStyle.STREETS,
     center: center || [0, 0],
-    zoom: 10
+    zoom: 10,
+    geolocate: true,
   });
+
+  // geocode control
+  const gc = new GeocodingControl({
+    enableReverse: 'always',
+    adjustUrl(url) {
+        // for reverse geocoding use only address type
+        if (/\/\d+\.?\d*%2C\d+.?\d*.json$/.test(url.pathname)) {
+          url.searchParams.set("types", "address");
+          url.searchParams.set("limit", "3");
+        }
+      },
+  })
+  map.value.addControl(gc, 'top-left')
 });
 
 onUnmounted(() => {
